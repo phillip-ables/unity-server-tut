32684f23f00df30aa49594c01d43d6d83567c569
lip ables <phillipables1@gmail.com>
2019-01-23 06:53:26 -0500

    instance of host topology connection config with private const int max users, network transport add host and websocket host of our topology instance to private constant int ports and null

--- serverLLAPI/Assets/Scripts/Server.cs
+++ serverLLAPI/Assets/Scripts/Server.cs
@@ -3,7 +3,11 @@ using UnityEngine.Networking;
 
 public class Server : MonoBehaviour {
     private byte reliableChannel;
-    
+    private const int MAX_USER = 100;
+    private const int PORT = 26000;
+    private const int WEB_PORT = 26001;
+
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
@@ -16,5 +20,11 @@ public class Server : MonoBehaviour {
         //how are  we gonna communicate through our rode
         ConnectionConfig cc = new ConnectionConfig();
         reliableChannel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology topo = new HostTopology(cc, MAX_USER);
+
+        // SERVER ONLY CODE
+        NetworkTransport.AddHost(topo, PORT, null);
+        NetworkTransport.AddWebsocketHost(topo, WEB_PORT, null);
     }
 }
