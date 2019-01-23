77d56ef07d5b6edf506fc339184602c828c6ab34
lip ables <phillipables1@gmail.com>
2019-01-23 01:41:41 -0500

    private byte, on init init network transport instance, new connection config add chanel type reliable and save as a byte

--- serverLLAPI/Assets/Scripts/Server.cs
+++ serverLLAPI/Assets/Scripts/Server.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
 public class Server : MonoBehaviour {
+    private byte reliableChannel;
+    
     private void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
+        Init();
     }
 
     public void Init()
     {
-
+        NetworkTransport.Init();
+        //how are  we gonna communicate through our rode
+        ConnectionConfig cc = new ConnectionConfig();
+        reliableChannel = cc.AddChannel(QosType.Reliable);
     }
 }
