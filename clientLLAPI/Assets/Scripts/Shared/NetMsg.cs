8defb1716bf08ab57f0dbc3cf74a68ca9850a515
lip ables <phillipables1@gmail.com>
2019-01-23 23:54:47 -0500

    public operational code that will be defined in public static class netop at the top of net msg

--- clientLLAPI/Assets/Scripts/Shared/NetMsg.cs
+++ clientLLAPI/Assets/Scripts/Shared/NetMsg.cs
@@ -1,8 +1,14 @@
-﻿
+﻿public static class NetOP
+{
+
+}
+
 [System.Serializable]  // using a binary writer and reader
 public abstract class NetMsg
 {
     //everything that is to be put in here is going to be put in a packet
     //as minimal as possible
+
+    public byte OP { set; get; }  // operational code
 
 }
