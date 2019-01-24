82d764a67c682381230eabd3c7dfec0871007e8b
lip ables <phillipables1@gmail.com>
2019-01-23 23:52:47 -0500

    never used on its own only want to see it inherited from so i declared as abstract

--- clientLLAPI/Assets/Scripts/Shared/NetMsg.cs
+++ clientLLAPI/Assets/Scripts/Shared/NetMsg.cs
@@ -1,6 +1,6 @@
 ﻿
 [System.Serializable]  // using a binary writer and reader
-public class NetMsg
+public abstract class NetMsg
 {
     //everything that is to be put in here is going to be put in a packet
     //as minimal as possible
